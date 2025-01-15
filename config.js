a4a0dd6920a5f7f89a32c6aab593660684d92dcd
Anant3008 <sastrulaanant@gmail.com>
2025-01-15 11:35:04 +0530

    minor changes

--- config.js
+++ config.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 const config = {
   apiKey: process.env.API_KEY,
   serverSecret: process.env.SERVER_SECRET,
-  isKalvian: process.env.IS_KALVIAN === 'true',
+  isKalvian: process.env.IS_KALVIAN === "true",
 };
+
+if( process.env.IS_KALVIAN === "true"){
+  console.log("true")
+}
+
 
 
 module.exports = config;
-
-console.log(config);
+console.log(config)
